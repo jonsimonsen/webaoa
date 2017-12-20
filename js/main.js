0de82ed8b92011aa1fe7af2dd8f9b2424352e722
Jon Simonsen <jonsimonsen@users.noreply.github.com>
2017-12-20 11:23:27 +0100

    Tempo commit after finishing initial JS for the banner.

--- js/main.js
+++ js/main.js
@@ -6,16 +6,42 @@ $(document).ready( () => {
   <a href="./index.html">
     <img src="Bilder/logo_aoa.jpg" alt="Logoen til Arbeid og Aktivitet">
   </a>
-  <nav class="nav-top">
-    <a href="./index.html#top">Hjem</a>
-    <a href="./dreis.html">Arbeid</a>
-    <a href="./aktiv.html">Aktivitet</a>
-    <a href="./index.html#employee">Ansatte</a>
-    <a href="./index.html#con_aoa">Kontakt</a>
-  </nav>
+  <nav class="nav-top">`;
+
+  let links = new Map();
+  links.set("./index.html", "Hjem");
+  links.set("./dreis.html", "Arbeid");
+  links.set("./aktiv.html", "Aktivitet");
+
+  const END = `</nav>
 </div>`;
 
-$(".banner-wrapper").append(START);
+  let total = START;
+  let contact = "";
+
+  links.forEach(function(text, address) {
+    total += '<a href="' + address;
+
+    if(window.location.pathname.endsWith(address.slice(1))) {
+      total += '" class="unlink';
+    }
+
+    total += '">' + text + '</a>';
+  });
+
+  if(window.location.pathname.endsWith("/index.html")) {
+    total += '<a href="#con_aoa">Kontakt</a>';
+  }
+  else if (window.location.pathname.endsWith("/dreis.html")) {
+    total += '<a href="#con_dreis">Kontakt</a>';
+  }
+  else {
+    total += '<a href="#" class="unlink">Kontakt</a>';
+  }
+
+  total += END;
+
+  $(".banner-wrapper").append(total);
 
   /*Add arrow symbol to each storylink*/
   $(".storylink").append('<div class="arrow"></div>');
