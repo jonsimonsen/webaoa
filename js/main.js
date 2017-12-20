739c0901cb84afe9a53d6990b552110529c19876
Jon Simonsen <jonsimonsen@users.noreply.github.com>
2017-12-20 12:12:43 +0100

    Did some cleanup in the JS file. What remains is to look into making common footer elements.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 /*Generate html for storylinks for user stories*/
 $(document).ready( () => {
 
-  /*Add banner*/
+  /*Banner*/
+
+  /*Site wide content*/
   const START = `<div class="banner">
   <a href="./index.html">
     <img src="Bilder/logo_aoa.jpg" alt="Logoen til Arbeid og Aktivitet">
   </a>
   <nav class="nav-top">`;
 
+  const END = `</nav>
+</div>`;
+
+  /*Map with link address as key and link text as value*/
   let links = new Map();
   links.set("./index.html", "Hjem");
   links.set("./dreis.html", "Arbeid");
   links.set("./aktiv.html", "Aktivitet");
 
-  const END = `</nav>
-</div>`;
-
   let total = START;
   let contact = "";
 
+  /*Add links, but give self-pointing links class unlink (not clickable)*/
   links.forEach(function(text, address) {
     total += '<a href="' + address;
 
@@ -29,6 +33,7 @@ $(document).ready( () => {
     total += '">' + text + '</a>';
   });
 
+  /*Add contact link. Should not be clickable if there is no contact info on the current page.*/
   if(window.location.pathname.endsWith("/index.html")) {
     total += '<a href="#con_aoa">Kontakt</a>';
   }
@@ -41,6 +46,7 @@ $(document).ready( () => {
 
   total += END;
 
+  /*Append the banner to its wrapper div*/
   $(".banner-wrapper").append(total);
 
   /*Add arrow symbol to each storylink*/
