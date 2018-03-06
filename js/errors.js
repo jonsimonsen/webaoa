a24b04369aab7e7e12d696414b8d8d1d4973b808
Jon Simonsen <jonsimonsen@users.noreply.github.com>
2018-03-06 13:12:24 +0100

    Tempo commit. It seems like testing importing of classes in an existing project is not the best way to approach it. Planning to look into making a new project for it insted, and rolling back or adapting the current code afterwards.

--- js/errors.js
+++ js/errors.js
@@ -1,7 +1,23 @@
 /*** JS file containing a class for errors.
 Originally intended for the website of "Arbeid og Aktivitet, Tromsø" (by Jon Simonsen). ***/
 
-class TestError extends Error{
+/**class TestError extends Error{
+  constructor(message, origin){**/
+    /*Initialize a new error using base constructor*/
+    /**super(message);
+    this._origin = origin;
+  }
+
+  logToConsole(){
+    console.log(`${this._origin}() error: ${this.message}`);
+  }
+}
+
+export default class TestError {}**/
+
+module.exports = {};
+
+module.exports.TestError = class TestError extends Error{
   constructor(message, origin){
     /*Initialize a new error using base constructor*/
     super(message);
@@ -12,5 +28,3 @@ class TestError extends Error{
     console.log(`${this._origin}() error: ${this.message}`);
   }
 }
-
-export default class TestError {}
